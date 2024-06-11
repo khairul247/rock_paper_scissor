e4309ca70ce12cfa28dadc0e9110d98aedd5e1ee
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2024-06-11 10:19:45 +0800

    Created function for computer's choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+console.log("Hello World");
+
+function getComputerChoice(){
+    compChoice=Math.round(Math.random()*2)
+
+    console.log(compChoice)
+
+    if (compChoice === 0){
+        compChoice = "rock";
+    } else if (compChoice === 1){
+        compChoice = "paper";
+    } else
+        compChoice = "scissors"
+    
+        return compChoice;
+}
+
+console.log(getComputerChoice());
